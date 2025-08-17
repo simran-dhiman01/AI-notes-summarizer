a9380a86afc2a8951d2a08aa77fe2455a267a677
simran-dhiman01 <simrankaurdh5@gmail.com>
2025-08-17 16:39:28 +0530

    production ready

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -5,7 +5,7 @@ import { generateSummary } from "../controllers/aiController.js";
 const router = express.Router();
 const upload = multer({
   storage: multer.memoryStorage(), // keeps file in memory instead of saving to disk
-  limits: { fileSize: 5 * 1024 * 1024 }, // optional: 5MB max
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB max
 });
 
 router.post("/", upload.single("file"),generateSummary);
